449eaab0e1fcd1dad9cfec84625d3f00d1ffbe9f
serenhanany <101177220+serenhanany@users.noreply.github.com>
2025-05-04 14:37:12 +0300

    update backend file

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,12 @@
 from db import db
+from datetime import datetime
 
 class User(db.Model):
-    __tablename__ = 'user' 
+    __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False) 
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.String(20))
-    password = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(100), nullable=False)
+    password_h = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
